1288d6c16f3e9d70744199fcb1bcf95a4ba266ad
thanhlnbka <thanhlnbka@gmail.com>
2024-01-08 00:02:44 +0700

    the first commit

---
+++ build/CMakeFiles/webrtc-sendrecv.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/webrtc-sendrecv.dir/webrtc-sendrecv.cpp.o"
+  "CMakeFiles/webrtc-sendrecv.dir/webrtc-sendrecv.cpp.o.d"
+  "webrtc-sendrecv"
+  "webrtc-sendrecv.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/webrtc-sendrecv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
